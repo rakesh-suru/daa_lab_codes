a5d3e767151523d16ed2b54f0941b19a7b1a43ea
rakesh-suru <152371111+rakesh-suru@users.noreply.github.com>
2025-07-24 20:44:11 +0530

    Update 3 c knapsack_dynamic.c

--- 3 c knapsack_dynamic.c
+++ 3 c knapsack_dynamic.c
@@ -1,39 +1,37 @@
-//Knapsack using dynamic method
-
 #include<stdio.h>
 
 int max(int a, int b){
-	return (a > b) ? a : b;
+    return (a > b) ? a : b;
 }
 
-int knapsack(int W, int weights[], int values[], int n){
-	int dp[n+1][W+1];
-	int i , w;
-	
-	for(i = 0 ; i<=n; i++){
-		for (w = 0; w<=W; w++){
-			if(i == 0 || w == 0){
-				dp[i][w] = 0;
-			}
-			else if(weights[i-1] <= w){
-				dp[i][w] = max(values[i-1] + dp[i-1][w-weights[i-1]], dp[i-1][w]);
-			}
-			else{
-				dp[i][w] = dp[i-1][w];
-			}
-		}
-	}
-	return dp[n][w];
+int knapsack(int W, int wt[], int val[], int n){
+    int dp[n+1][W+1];
+    int i, w;
+
+    for(i = 0; i <= n; i++){
+        for(w = 0; w <= W; w++){
+            if (i == 0 || w == 0){
+                dp[i][w] = 0;
+            }
+            else if(wt[i-1] <= w){
+                dp[i][w] = max(val[i-1] + dp[i-1][w - wt[i-1]], dp[i-1][w]);
+            }
+            else{
+                dp[i][w] = dp[i-1][w];
+            }
+        }
+    }
+    return dp[n][W];
 }
 
 int main(){
-	int values[] = {60, 100, 120};
-	int weights[] = {10, 20, 30};
-	int capacity = 50;
-	int n = sizeof(values)/sizeof(values[0]);
-	
-	int max_value = knapsack(capacity, weights, values, n);
-	printf("Maximum value in knapsack : %d\n", max_value);
-	
-	return 0;
+    int val[] = {60, 100, 120};
+    int wt[] = {10, 20, 30};
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+
+    int max_value = knapsack(W, wt, val, n);
+    printf("Maximum Value in knapsack = %d\n", max_value);
+
+    return 0;
 }
